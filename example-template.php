dab0b0333b4ba961b8211a269ed9c0bf9b487d44
David Harris <git@dsharris.org>
2015-01-29 11:57:50 -0600

    added the page attribute

--- example-template.php
+++ example-template.php
@@ -8,20 +8,24 @@
 <section class="section-main" role="main">
 
 	<div class="grid infinite-scroll"
+		<?php // if you are preloading the first set of results set page, otherwise leave it out ?>
+		data-page="2"
+
 		data-posts-per-page="3"
 		data-cleaner="clean_data"
 		data-taxonomy-term="<?php echo $vars->term_id ?>"
-		data-taxonomy-name="<?php echo $vars->taxonomy ?>"
-	>
+		data-taxonomy-name="<?php echo $vars->taxonomy ?>">
 
-	<?php
-		// InfiniteScroll::inst() will load the already declared instance rather than build a new one
-		InfiniteScroll::inst()
-			// this will load _ (underscore) from a CDN, i only do this because browserify kill it
-			->load_underscore()
-			// this will echo the template on the page
-			->get_template('post-template');
-	?>
+		<?php
+			// InfiniteScroll::inst() will load the already declared instance rather than build a new one
+			InfiniteScroll::inst()
+				// this will load _ (underscore) from a CDN, i only do this because browserify kill it
+				->load_underscore()
+				// this will echo the template on the page
+				->get_template('post-template');
+		?>
+
+	</div>
 
 </section>
 
